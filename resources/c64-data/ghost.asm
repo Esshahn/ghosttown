abc76c6d7d6562713204743921fd5c362e693226
Ingo Hinterding <ingo@hinterding.de>
2015-08-11 17:11:22 +0200

    still testing

--- resources/c64-data/ghost.asm
+++ resources/c64-data/ghost.asm
@@ -2,22 +2,23 @@
 ; 	awsm 2015
 
 
-!to "build/ghostr.prg",cbm
+!to "build/ghost.prg",cbm
 
 * = $0801                               ; BASIC start address (#2049)
 !byte $0d,$08,$dc,$07,$9e,$20,$31,$32   ; BASIC loader to start at $c000...
 !byte $32,$38,$38,$00,$00,$00           ; puts BASIC line 2012 SYS 49152
 
 *=$0c00
-!bin "screen-.bin",,2
+!bin "screen-ghost.bin",,2
 
 *=$1800
-!bin "col-.bin",,2
+!bin "col-ghost.bin",,2
 
 *=$2000
-!bin "ghostchar.bin",,2
+!bin "char-ghost.bin",,2
 
 *=$3000
+
 
 ; copy color ram to new position
 	ldx #$00
@@ -33,7 +34,7 @@
 	bne -
 
 ; background color = black
-	lda #$01
+	lda #$00
 	sta $d021
 
 ; border color = red
