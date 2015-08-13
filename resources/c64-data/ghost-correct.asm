fa6b42de5b6800e83591878500a105dfb1003bf8
Ingo Hinterding <ingo@hinterding.de>
2015-08-13 22:49:18 +0200

    more psd

--- resources/c64-data/ghost-correct.asm
+++ resources/c64-data/ghost-correct.asm
@@ -2,17 +2,17 @@
 ; 	awsm 2015
 
 
-!to "build/ghost.prg",cbm
+!to "build/ghost-correct.prg",cbm
 
 * = $0801                               ; BASIC start address (#2049)
 !byte $0d,$08,$dc,$07,$9e,$20,$31,$32   ; BASIC loader to start at $c000...
 !byte $32,$38,$38,$00,$00,$00           ; puts BASIC line 2012 SYS 49152
 
 *=$0c00
-!bin "screen-abc.bin",,2
+!bin "screen-ghost.bin",,2
 
 *=$1800
-!bin "col-black.bin",,2
+!bin "col-ghost.bin",,2
 
 *=$2000
 !bin "char-ghost.bin",,2
