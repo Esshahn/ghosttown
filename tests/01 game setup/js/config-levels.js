2e10f9a323a28c3d220e06419a67077a854ff2bf
Ingo Hinterding <ingo@hinterding.de>
2015-09-03 17:10:08 +0200

    schit all deleted

---
+++ tests/01 game setup/js/config-levels.js
@@ -0,0 +1,43 @@
+
+levels = {
+
+
+    1: { filename: "01", oranges: 4 }, 
+    2: { filename: "02", oranges: 4 },
+    3: { filename: "03", oranges: 4 },
+    4: { filename: "04", oranges: 4 }, 
+    5: { filename: "05", oranges: 4 },
+    6: { filename: "06", oranges: 4 },
+    7: { filename: "07", oranges: 4 }, 
+    8: { filename: "08", oranges: 4 },
+    9: { filename: "09", oranges: 4 },
+    10: { filename: "10", oranges: 4 }, 
+    11: { filename: "11", oranges: 4 },
+    12: { filename: "12", oranges: 4 },
+    13: { filename: "13", oranges: 4 }, 
+    14: { filename: "14", oranges: 4 },
+    15: { filename: "15", oranges: 4 },
+    16: { filename: "16", oranges: 4 }, 
+    17: { filename: "17", oranges: 4 },
+    18: { filename: "18", oranges: 4 },
+    19: { filename: "19", oranges: 4 }, 
+    20: { filename: "20", oranges: 4 }
+
+};
+
+
+console.log(levels[17].oranges);
+
+
+
+/*
+
+
+for (var i = 0; i<20; i++){
+  var level_number = ("0" + (i+1) ).slice(-2);
+  //levels[i].filename = "data/screen-lvl-"+ level_number +".bin";
+  levels[i].filename = "hlllo";
+ // console.log(levels[i].filename);
+}
+
+*/
---
+++ tests/01 game setup/js/level.js
@@ -0,0 +1,71 @@
+/*
+
+  GHOST TOWN JS
+  LEVEL GENERATOR
+
+*/
+
+
+
+console.log(levels.filename);
+
+Level = function(filename){
+  console.log("creating level");
+  this.filename = filename;
+  this.screen = import_screen_data(this.filename);
+
+};
+
+
+display_level = function (level_data){
+  var level_sprites = [];
+  var xpos          = 0;
+  var ypos          = 0;
+
+  for(i=0; i<level_data.length;i++){
+    level_sprites[i] = new PIXI.Sprite(charset[level_data[i]]);
+    if (xpos >= SCREEN_WIDTH){
+      xpos = 0;
+      ypos += 8;
+    }
+    level_sprites[i].position.x = xpos;
+    level_sprites[i].position.y = ypos;
+    screen.addChild(level_sprites[i]);
+
+    xpos += 8;
+  }
+};
+
+import_screen_data = function (filename){
+
+  // loads in a level binary file
+
+  var level_data = [];
+
+  var oReq = new XMLHttpRequest();
+  oReq.open("GET", filename, true);
+  oReq.responseType = "arraybuffer";
+
+  oReq.onload = function (oEvent) {
+    var arrayBuffer = oReq.response; // Note: not oReq.responseText
+    if (arrayBuffer) {
+      var byteArray = new Uint8Array(arrayBuffer);
+      // start i=2 -> skip the first two values, they are not needed
+      for (var i = 2; i < byteArray.byteLength; i++) {
+        
+        // convert the number to 2 digit hex
+        char = ("0"+byteArray[i].toString(16)).slice(-2);
+        
+        // erase the player character from the level data
+        if (char >= "93" && char <= "9b") char = "df";
+        
+        // put all hex data in level_data
+        level_data.push(char);
+      }
+    }   
+  };
+
+  oReq.send(null); 
+  return level_data;
+  
+};
