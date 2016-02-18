885f7576ba50326a69321ee6429b4dacbda412db
Ingo Hinterding <ingo@hinterding.de>
2016-02-18 10:45:16 +0100

    cleanup

--- older versions/01 game setup/js/asset-loader.js
+++
@@ -1,9 +0,0 @@
-
-function load_assets(){
-  var loader = PIXI.loader;
-
-  loader.add('bg_texture',"img/start.png");
-  loader.add('chars',"img/chars1.png");
-  loader.once('complete', asset_loader_ready);
-  loader.load();
-}
--- older versions/02 coffee conversion/coffee/asset-loader.coffee
+++
@@ -1,9 +0,0 @@
-
-
-load_assets = -> 
-  loader = PIXI.loader
-  loader.add 'bg_texture', 'img/start.png'
-  loader.add 'chars', 'img/chars1.png'
-  loader.once 'complete', asset_loader_ready
-  loader.load()
-  return
